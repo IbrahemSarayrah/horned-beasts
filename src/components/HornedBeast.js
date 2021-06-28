e01ff06fba1f5fb2d459d123af84c3cb77077c40
IbrahemSarayrah <ibrahem.sarayrah@gmail.com>
2021-06-28 17:38:56 +0300

    finish-lab02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 class HornedBeast extends React.Component {
-    render(){
-        return(
-            <div className='imgDiv'>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt={this.props.title} className='img'/>
-                <p>{this.props.description}</p>
-            </div>
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            numberOfLike: 0
+        }
+    }
+    increaseLike = () => {
+        this.setState({
+            numberOfLike: this.state.numberOfLike + 1
+        })
+    }
+
+
+    render() {
+        return (
+            <Card style={{ width: '18rem', margin: '20px', textAlign: 'center' }}>
+                <Card.Img variant="top"
+                    src={this.props.image_url}
+                    alt={this.props.title}
+                    className='img'
+                />
+                <Card.Body style={{ color: 'white', backgroundColor: '#053742' }}>
+                    <Card.Title >{this.props.title}</Card.Title>
+                    <Card.Text >
+                        {this.props.description}
+                    </Card.Text>
+                    <Button onClick={this.increaseLike} variant="primary">Like  ❤️ {this.state.numberOfLike}</Button>
+                </Card.Body>
+            </Card>
         )
     }
 }
